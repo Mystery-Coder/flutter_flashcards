19e1311e17e44685c2f77c28d4cf43287d8be732
MysteryCoder <srikar0811@gmail.com>
2025-08-29 12:21:57 +0530

    db setup

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_flashcards/db_helper.dart';
+import 'package:http/http.dart' as http;
 // import 'package:sqflite/sqlite_api.dart';
 
 class Home extends StatefulWidget {
@@ -10,28 +13,34 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
-  void initState() {
-    super.initState();
+  // @override
+  // void initState() {
+  //   super.initState();
 
-    testSQL() async {
-      final DatabaseHelper singleInstance = DatabaseHelper.singleInstance;
+  //   testSQL() async {
+  //     final DatabaseHelper singleInstance = DatabaseHelper.singleInstance;
 
-      await singleInstance.rawInstert(
-          FlashCardData(question: "SQL?", answer: "Structured Query Language"));
-      await singleInstance
-          .rawInstert(FlashCardData(question: "JS?", answer: "JavaScript"));
+  //     await singleInstance.rawInstert(
+  //         FlashCardData(question: "SQL?", answer: "Structured Query Language"));
+  //     await singleInstance
+  //         .rawInstert(FlashCardData(question: "JS?", answer: "JavaScript"));
 
-      final List<FlashCardData> flashcards =
-          await singleInstance.rawQueryAllRows();
+  //     final List<FlashCardData> flashcards =
+  //         await singleInstance.rawQueryAllRows();
+  //     var res = await http.get(Uri.parse("http://10.0.2.2:8080/"));
+  //     var data = jsonDecode(res.body);
+  //     print(data);
+  //     for (var i = 0; i < flashcards.length; i++) {
+  //       print(flashcards[i].id.toString() +
+  //           " " +
+  //           flashcards[i].question +
+  //           " " +
+  //           flashcards[i].answer);
+  //     }
+  //   }
 
-      for (var i = 0; i < flashcards.length; i++) {
-        print(flashcards[i].question + " " + flashcards[i].answer);
-      }
-    }
-
-    testSQL();
-  }
+  //   testSQL();
+  // }
 
   @override
   Widget build(BuildContext context) {
